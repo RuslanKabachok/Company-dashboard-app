7846ea0075d6172e858c07f43bf8d3b70e692693
RuslanKabachok <ruslan.kabachok@gmail.com>
2025-06-30 18:12:24 +0300

    upd

--- server/index.js
+++ server/index.js
@@ -25,7 +25,6 @@ app.use(cors({
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/admin', adminRoutes);
